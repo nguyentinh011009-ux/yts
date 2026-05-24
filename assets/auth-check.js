efa7eab0f5a9941aa1312803b435a4a792e3cad3
nguyentinh011009-ux <nguyentinh011009@gmail.com>
2026-05-24 08:29:06 +0700

    Add admin email constants for authentication checks

---
+++ assets/auth-check.js
@@ -0,0 +1,8 @@
+//assets/auth-check.js
+const ALLOWED_ADMIN_EMAILS = [
+    "nguyentinh011009@gmail.com",
+     "tomizy09icloud@gmail.com",
+     "nguyenthixuandongvts@gmail.com",
+     "yte.thptvothisaubrvt@gmail.com",
+     "nguyentinh52009@gmail.com"
+];
